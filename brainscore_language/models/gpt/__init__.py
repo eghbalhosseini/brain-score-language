dc5367900dfb0ffbd2018b1e0536bc921bbe5005
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2022-11-16 15:07:58 -0500

    debugging problem with importing models

--- brainscore_language/models/gpt/__init__.py
+++ brainscore_language/models/gpt/__init__.py
@@ -23,7 +23,7 @@ model_registry['distilgpt2-Pereira2018'] = lambda: HuggingfaceSubject(model_id='
     ArtificialSubject.RecordingTarget.language_system: 'transformer.h.5'})
 
 
-model_registry[f'distilgpt2-layerwise'] = lambda: HuggingfaceGroup(model_id='distilgpt2', region_layer_mapping={
+model_registry['distilgpt2-layerwise'] = lambda: HuggingfaceGroup(model_id='distilgpt2', region_layer_mapping={
     ArtificialSubject.RecordingTarget.language_system: get_layer_names('distilgpt2')})
 
 
