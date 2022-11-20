e94e0babb441e5d960827e7e58b59532fb95bb25
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2022-11-20 16:41:44 -0500

    fixes on benchmark ceiling arguments

--- brainscore_language/models/xlnet/modeling.py
+++ brainscore_language/models/xlnet/modeling.py
@@ -11,7 +11,8 @@ import re
 def get_layer_names(model_id):
     model_ = AutoModelForCausalLM.from_pretrained(model_id)
     modul_names = [x[0] for x in model_.named_modules()]
-    layer_drop_names = [x for x in modul_names if len(re.findall(r'.word_embedding$', x)) > 0]
+    #layer_drop_names = [x for x in modul_names if len(re.findall(r'.word_embedding$', x)) > 0]
+    layer_drop_names = []
     layer_output_names = [x for x in modul_names if len(re.findall(r'transformer.layer.\d+$', x)) > 0]
     flat_list = [item for sublist in [layer_drop_names,layer_output_names] for item in sublist]
     return flat_list
