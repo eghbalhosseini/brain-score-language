3700cab3027a089871f0cb504b337b6cefc7b324
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2023-01-05 09:52:54 -0500

    adding bert base uncased whole word masking

--- brainscore_language/models/bert/__init__.py
+++ brainscore_language/models/bert/__init__.py
@@ -2,6 +2,10 @@ from brainscore_language import model_registry
 from brainscore_language import ArtificialSubject
 from .modeling import BERTSubject, BERTGroup, get_layer_names
 
+model_registry['bert-large-uncased-whole-word-masking'] = lambda: BERTSubject(model_id="bert-large-uncased-whole-word-masking", region_layer_mapping={
+    ArtificialSubject.RecordingTarget.language_system: 'bert.encoder.layer.11.output'})
+
+
 model_registry['bert-base-uncased'] = lambda: BERTSubject(model_id="bert-base-uncased", region_layer_mapping={
     ArtificialSubject.RecordingTarget.language_system: 'bert.encoder.layer.11.output'})
 
