5ef022ee1b62f0f6c0a7ac81a9e88134c3c9b822
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2023-06-19 14:56:39 -0400

    adding DsParametric

--- bash/om_run_models_on_DsParametric_fMRI_benchmark.sh
+++ bash/om_run_models_on_DsParametric_fMRI_benchmark.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #SBATCH --job-name=ANNSet1
-#SBATCH -t 2-12:00:00
+#SBATCH -t 18:00:00
 #SBATCH -c 3
-#SBATCH --mem=120G
+#SBATCH --mem=20G
 #SBATCH --exclude node017,node018
 #SBATCH --mail-type=ALL
 
@@ -15,4 +15,4 @@ echo $(which python)
 ATLAS_ID=$1
 echo "model_id:${ATLAS_ID}"
 
-python /rdma/vast-rdma/vast/evlab/ehoseini/brain-score-language/analysis/run_model_against_ANN_benchmarks.py "$ATLAS_ID"
+python /rdma/vast-rdma/vast/evlab/ehoseini/brain-score-language/analysis/run_model_against_DsParametric_fMRI_benchmarks.py "$ATLAS_ID"
